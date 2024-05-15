2c0ca3f487932eec2452288e65a6aff24a2b247c
John Edwards <edwards1860@gmail.com>
2024-05-15 10:44:08 -0700

    data dictionary update

--- data-raw/build_data_dictionaries.R
+++ data-raw/build_data_dictionaries.R
@@ -1,5 +1,25 @@
 setwd(here::here())
 
+pull_teams <- function(league_id) {
+  glue::glue("https://api.actionnetwork.com/web/v1/leagues/{league_id}/teams") |>
+  jsonlite::fromJSON() |>
+    purrr::pluck("teams") |>
+    dplyr::select(
+      id,
+      full_name,
+      display_name,
+      short_name,
+      location,
+      abbr,
+      logo,
+      primary_color,
+      secondary_color,
+      conference_type,
+      division_type,
+      since
+    )
+}
+
 # Build books dataframe ----
 
 books <-
@@ -29,90 +49,104 @@ usethis::use_data(books, overwrite = T)
 
 # Build NFL teams dataframe ----
 
-nfl_teams <-
-  jsonlite::fromJSON("https://api.actionnetwork.com/web/v1/leagues/1/teams") |>
-  purrr::pluck("teams") |>
-  dplyr::select(
-    id,
-    full_name,
-    display_name,
-    short_name,
-    location,
-    abbr,
-    logo,
-    primary_color,
-    secondary_color,
-    conference_type,
-    division_type,
-    since
-  )
+nfl_teams <- pull_teams(1)
 
 usethis::use_data(nfl_teams, overwrite = T)
 
 # Build CFB teams dataframe ----
 
-cfb_teams <-
-  jsonlite::fromJSON("https://api.actionnetwork.com/web/v1/leagues/2/teams") |>
-  purrr::pluck("teams") |>
-  dplyr::select(
-    id,
-    full_name,
-    display_name,
-    short_name,
-    location,
-    abbr,
-    logo,
-    primary_color,
-    secondary_color,
-    conference_type,
-    division_type,
-    since
-  )
+cfb_teams <- pull_teams(2)
 
 usethis::use_data(cfb_teams, overwrite = T)
 
+# build NHL teams dataframe ----
+
+nhl_teams <- pull_teams(3)
+
+usethis::use_data(nhl_teams, overwrite = T)
+
 # build NBA teams dataframe ----
 
-nba_teams <-
-  jsonlite::fromJSON("https://api.actionnetwork.com/web/v1/leagues/4/teams") |>
-  purrr::pluck("teams") |>
-  dplyr::select(
-    id,
-    full_name,
-    display_name,
-    short_name,
-    location,
-    abbr,
-    logo,
-    primary_color,
-    secondary_color,
-    conference_type,
-    division_type,
-    since
-  )
+nba_teams <- pull_teams(4)
 
 usethis::use_data(nba_teams, overwrite = T)
 
+# build WNBA teams dataframe ----
+
+wnba_teams <- pull_teams(5)
+
+usethis::use_data(wnba_teams, overwrite = T)
+
 # build NCAAB teams dataframe ----
 
-ncaab_teams <-
-  jsonlite::fromJSON("https://api.actionnetwork.com/web/v1/leagues/6/teams") |>
-  purrr::pluck("teams") |>
-  dplyr::select(
-    id,
-    full_name,
-    display_name,
-    short_name,
-    location,
-    abbr,
-    logo,
-    primary_color,
-    secondary_color,
-    conference_type,
-    division_type,
-    since
-  )
+ncaab_teams <- pull_teams(6)
 
 usethis::use_data(ncaab_teams, overwrite = T)
 
-# todo: NHL, WNBA, MLB, Foreign soccer leagues, World Cup, XFL?
+# build WNCAAB teams dataframe ----
+
+wncaab_teams <- pull_teams(7)
+
+usethis::use_data(wncaab_teams, overwrite = T)
+
+# build MLB teams dataframe -----
+
+mlb_teams <- pull_teams(8)
+
+usethis::use_data(mlb_teams, overwrite = T)
+
+# build Premiere League teams dataframe ----
+
+pl_teams <- pull_teams(9)
+
+usethis::use_data(pl_teams, overwrite = T)
+
+# build Bundesliga teams dataframe ----
+
+bundesliga_teams <- pull_teams(10)
+
+usethis::use_data(bundesliga_teams, overwrite = T)
+
+# build Ligue teams dataframe ----
+
+ligue_teams <- pull_teams(11)
+
+usethis::use_data(ligue_teams, overwrite = T)
+
+# build La Liga teams dataframe ----
+
+la_liga_teams <- pull_teams(12)
+
+usethis::use_data(la_liga_teams, overwrite = T)
+
+# build Serie teams dataframe ----
+
+serie_teams <- pull_teams(13)
+
+usethis::use_data(serie_teams, overwrite = T)
+
+# build MLS teams dataframe ----
+
+mls_teams <- pull_teams(14)
+
+usethis::use_data(mls_teams, overwrite = T)
+
+# build world cup dataframe -----
+wc_teams <- pull_teams(20)
+
+usethis::use_data(wc_teams, overwrite = T)
+
+# build women's world cup dataframe -----
+wwc_teams <- pull_teams(34)
+
+usethis::use_data(wwc_teams, overwrite = T)
+
+# build aaf dataframe -----
+aaf_teams <- pull_teams(21)
+
+usethis::use_data(aaf_teams, overwrite = T)
+
+# build usfl dataframe -------
+aaf_teams <- pull_teams(24)
+
+usethis::use_data(aaf_teams, overwrite = T)
